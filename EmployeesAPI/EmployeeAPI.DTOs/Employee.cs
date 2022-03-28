33d8258f06605e0d95942914ba852211a745ef79
Ricardo Tondello <rkdtondello@gmail.com>
2022-03-28 19:32:11 +0200

    add skeleton

---
+++ EmployeesAPI/EmployeeAPI.DTOs/Employee.cs
@@ -0,0 +1,19 @@
+﻿namespace EmployeeAPI.DTOs
+{
+    public class Employee
+    {
+        public Guid Id { get; }
+        public string Name { get; }
+        public Region Region { get;  }
+
+        private Employee(Guid id, string name, Region region)
+        {
+            Id = id;
+            Name = name;
+            Region = region;
+        }
+
+        public static Employee Create(Guid id, string name, Region region) =>
+            new Employee(Guid.NewGuid(), name, region);
+    }
+}
---
+++ EmployeesAPI/EmployeesAPI.Entities/Region.cs
@@ -0,0 +1,18 @@
+﻿namespace EmployeesAPI.Entities
+{
+    public class Region
+    {
+        public int Id { get; }
+        public string Name { get; }
+        public Region Parent { get; }
+
+        private Region(int id, string name, Region parent)
+        {
+            Id = id;
+            Name = name;
+            Parent = parent;
+        }
+
+        public static Region Create(int id, string name, Region parent) => new Region(id, name, parent);
+    }
+}
